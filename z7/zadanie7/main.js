a98edbc13df1321c45f07a558733ac236418f7c7
s22051-pj-lukasz-kos <s22051@pjwstk.edu.pl>
2022-01-02 23:46:43 +0100

    Wykonane zadanie w całości dla pola 3x3

--- z7/zadanie7/main.js
+++ z7/zadanie7/main.js
@@ -2,17 +2,28 @@
 $(() => {
     console.log("jquery");
 
-    // nasłuch na kafelki
-    $(".container div").on("click", () => {
-        // nasłuch na sąsiadujące z pustą przestrzenią kafelki
-        $(".sasiaduje").on("click", (e) => move(e));
+    // tablica z pierwotnymi pozycjami obrazków
+    const arrayCorrect = ["obrazek1", "obrazek2", "obrazek3", "obrazek4", "obrazek5", "obrazek6", "obrazek7", "obrazek8", "pusty" ];
+    
+    // tablica na której wykonane jest przetasowanie
+    let array = [...arrayCorrect];
 
-        // usunięcie nasłuchu
-        // $(".sasiaduje").off();
+    // przetasowanie tablicy
+    shuffleArray(array);
+
+    // przypisanie odpowiednich klas, czyli przetasowanie obrazków
+    shufflePictures(array);
+
+    // nasłuch na kafelki
+    $(".container").on("click", ".sasiaduje", (e) => {
+        // przesunięcie kafelki
+        move(e);
 
         // usuwa klasy .sasiaduje i przelicza ich wystepowanie na nowo
         neighborChecker();
 
+        // sprawdza czy kafelki są ułożone w odpowiedniej kolejności
+        winChecker(arrayCorrect);
     });
     
     // przesuwanie kafelka
@@ -52,48 +63,50 @@ $(() => {
 
         // i dodaje odpowiednią klasę w zależności od występowania pustej kafli
         switch(pos) {
-            case 1: 
+            case "1": 
                 $("#div2").addClass("sasiaduje");
                 $("#div4").addClass("sasiaduje");
                 break;
-            case 2:
+            case "2":
                 $("#div1").addClass("sasiaduje");
                 $("#div3").addClass("sasiaduje");
                 $("#div5").addClass("sasiaduje");
                 break;
-            case 3:
+            case "3":
                 $("#div2").addClass("sasiaduje");
                 $("#div6").addClass("sasiaduje");
                 break;
-            case 4:
+            case "4":
                 $("#div1").addClass("sasiaduje");
                 $("#div5").addClass("sasiaduje");
                 $("#div7").addClass("sasiaduje");
                 break;
-            case 5:
+            case "5":
                 $("#div2").addClass("sasiaduje");
                 $("#div4").addClass("sasiaduje");
                 $("#div6").addClass("sasiaduje");
                 $("#div8").addClass("sasiaduje");
                 break;
-            case 6:
+            case "6":
                 $("#div3").addClass("sasiaduje");
                 $("#div5").addClass("sasiaduje");
                 $("#div9").addClass("sasiaduje");
                 break;
-            case 7: 
+            case "7": 
                 $("#div4").addClass("sasiaduje");
                 $("#div8").addClass("sasiaduje");
                 break;
-            case 8:
+            case "8":
                 $("#div5").addClass("sasiaduje");
                 $("#div7").addClass("sasiaduje");
                 $("#div9").addClass("sasiaduje");
                 break;
-            case 9:
+            case "9":
                 $("#div6").addClass("sasiaduje");
                 $("#div8").addClass("sasiaduje");
                 break;
+            default :
+                console.log("Dziwny zwrot z returnPosition.")
         }
     }
 
@@ -104,5 +117,53 @@ $(() => {
         return num;
     }
 
+    // funkcja do tasowania tablicy
+    function shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    }
+    
+    // funkcja do tasowania obrazków
+    function shufflePictures(array) {
+        // usuń wszystkie klasy
+        $(".container div").removeClass();
+        
+        // stwórz listę divów
+        let divs = document.querySelectorAll(".container div");
 
+        // przypisz im klasy z tablicy
+        for (let index = 0; index < array.length; index++) {
+            if(array[index] == "pusty") {
+                divs[index].classList.add("pusty");
+            } else {
+                divs[index].classList.add(array[index], "obrazek");
+            } 
+        }
+
+        // na końcu ustaw klasy .sasiaduje
+        neighborChecker();
+    }
+
+    // funkcja do sprawdzania czy wygrałeś
+    function winChecker(array) {
+        let flag = true;
+        
+        // stwórz listę divów
+        let divs = document.querySelectorAll(".container div");
+
+        // sprawdź kolejność z przekazaną tablicą
+        for (let index = 0; index < array.length; index++) {
+            if (!divs[index].classList.contains(array[index])) {
+                flag = false;
+                break;
+            }
+        }
+
+        // jeśli kolejność się zgadza z tablicą to komunikat
+        if (flag) {
+            alert("BRAWO!!! Dokonałeś niemożliwego!!!");
+        }
+    }
 });
